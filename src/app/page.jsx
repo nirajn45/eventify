4db40b929bbe2fef5d2d7ffd4d4be34ccce812c7
nirajn45 <guptaniraj915@gmail.com>
2024-10-10 14:52:04 +0530

    starting eventify

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react';
+
+
+const page = () => {
+    return (
+       <div>
+        <h6>abhi bss collab krne keliye krr rkha hai baki chije isme bad me paste krunga</h6>
+       </div>
+    )
+}
+
+export default page
